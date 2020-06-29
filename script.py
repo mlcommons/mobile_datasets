f613f782f1e5913ab8546e34d983615d3c54926d
cindytrinh1 <cindy.trinh.pro@gmail.com>
2020-06-29 17:00:51 +0200

    added balanced subsampling, constant url, ask before downloading

--- script.py
+++ script.py
@@ -1,16 +1,3 @@
-import argparse
-import os
-import random
-import shutil
-import urllib
-import zipfile
-import requests
-import subprocess
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 """
 Description:
@@ -28,7 +15,7 @@ Remarks:
 Example list of commands for using ADE20K as classification test dataset:
 ```
 git clone https://github.com/mlperf/mobile_app.git
-python script.py --mobile_app_path=./mobile_app --N=300 --dataset=ADE20K --type=imagenet
+python script.py --mobile_app_path=./mobile_app --N=300 --dataset=ADE20K --type=imagenet --subsampling_strategy=random
 export ANDROID_HOME=Path/to/SDK # Ex: $HOME/Android/Sdk
 export ANDROID_NDK_HOME=Path/to/NDK # Ex: $ANDROID_HOME/ndk/(your version)
 bazel-2.2.0 build -c opt --cxxopt='--std=c++14' \
@@ -37,6 +24,29 @@ bazel-2.2.0 build -c opt --cxxopt='--std=c++14' \
 adb install -r bazel-bin/java/org/mlperf/inference/mlperf_app.apk
 ```
 """
+
+import argparse
+import os
+import random
+import shutil
+import urllib
+import zipfile
+import requests
+import subprocess
+import logging
+from collections import defaultdict
+from math import ceil
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# CONSTANTS
+IMAGENET_CLASSES_URL = "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
+
+ADE20K_URL =  'https://github.com/ctrnh/mlperf_misc/raw/master/ADE20K_subset.zip' ## For development, can use this link, otherwise it'll download the entire dataset (3GB)
+#ADE20K_URL = "https://groups.csail.mit.edu/vision/datasets/ADE20K/ADE20K_2016_07_26.zip"
+
+
 
 class InputDataset:
     """
@@ -90,7 +100,7 @@ class InputDataset:
         """
         while os.path.isdir(self.tmp_path):
             if not self.yes_all:
-                choice_tmp = input(f"{self.tmp_path} already exists. Do you want to:\n 1. Remove this folder \n 2. Create another temporary folder? [1/2] \n")
+                choice_tmp = input(f"{self.tmp_path} could not be created, folder already exists. Do you want to:\n 1. Remove this folder \n 2. Create another temporary folder? [1/2] \n")
             if self.yes_all or choice_tmp == "1":
                 rm_tmp = subprocess.run(["rm", "-r", self.tmp_path],
                                         stderr=subprocess.PIPE,
@@ -103,17 +113,15 @@ class InputDataset:
                 new_tmp = input("Enter a name for the new tmp folder: ")
                 self.tmp_path = os.path.join(self.mobile_app_path, new_tmp)
             else:
-                logger.error("Please enter a valid choice (1 or 2)")
+                logger.error("Please enter a valid answer (1 or 2).")
 
     def load_classes(self):
         """
         Loads classes labels and indices depending on self.type
         """
         if self.type == "imagenet":
-            #url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
-            url = "https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
             logger.info("Loading imagenet classes")
-            self.IMAGENET_CLASSES =  {v:k for (k,v) in eval(requests.get(url).text).items()}
+            self.IMAGENET_CLASSES =  {v:k for (k,v) in eval(requests.get(IMAGENET_CLASSES_URL).text).items()}
             logger.debug(self.IMAGENET_CLASSES)
 
     def download_dataset(self):
@@ -193,7 +201,7 @@ class InputDataset:
                 elif delete == "n":
                     logger.error("Cannot pursue without removing those elements.")
                 else:
-                    logger.error("Please enter a valid choice (y or n)")
+                    logger.error("Please enter a valid answer (y or n).")
 
         logger.info(f"Creating {os.path.join(mobile_dataset_path,'img')} directory on the phone.")
         create_dir = subprocess.run(["adb", "shell", "mkdir", "-p", os.path.join(mobile_dataset_path,"img")])
@@ -202,7 +210,7 @@ class InputDataset:
 
         assert create_dir.returncode == push.returncode == 0
         if not self.yes_all:
-            remove_tmp_folder = input(f"Do you want to remove the temporary folder located at {self.tmp_path} created by the script? [y/n] \n")
+            remove_tmp_folder = input(f"Do you want to remove the temporary folder located at {self.tmp_path} which has been created by the script? [y/n] \n")
         if self.yes_all or remove_tmp_folder == 'y':
             clean = subprocess.run(["rm", "-r", self.tmp_path])
             logger.info(f"{self.tmp_path} folder has been removed.")
@@ -238,54 +246,81 @@ class ADE20KDataset(InputDataset):
         self.in_annotations = {}
 
     def download_dataset(self):
-        DEV = False
-        if DEV: # TODO: remove
-            dataset_name = "ADE20K_subset"
-            ADE20K_url =  'https://github.com/ctrnh/mlperf_misc/raw/master/ADE20K_subset.zip'
-        else:
-            dataset_name = "ADE20K_2016_07_26"
-            ADE20K_url = "https://groups.csail.mit.edu/vision/datasets/ADE20K/ADE20K_2016_07_26.zip"
+        dataset_name = ADE20K_URL.split("/")[-1].split(".")[0]
+        req = urllib.request.Request(ADE20K_URL, method="HEAD")
+        size_file = urllib.request.urlopen(req).headers["Content-Length"]
+        download = "n"
+        while download != "y":
+            if not self.yes_all:
+                download = input(f"You are about to download {dataset_name} ({size_file} bytes) to the temporary folder {self.tmp_path}. Do you want to continue? [y/n] \n")
+            if self.yes_all or download == "y":
+                logger.info(f"Downloading dataset {dataset_name} at {ADE20K_URL} to temporary folder {self.tmp_path}...")
+                zip_path, hdrs = urllib.request.urlretrieve(ADE20K_URL, f"{self.tmp_path}/{dataset_name}.zip")
+                logger.info(f"Extracting {zip_path} to temporary folder {self.tmp_path}...")
+                with zipfile.ZipFile(f"{zip_path}", 'r') as z:
+                    z.extractall(f"{self.tmp_path}")
+                self.input_data_path = zip_path[:-4]
+                break
+            elif download == "n":
+                print(f"Cannot pursue without downloading the dataset.")
+            else:
+                print(f"Please enter a valid answer (y or n).")
 
-        logger.info(f"Downloading dataset {dataset_name} at {ADE20K_url} to temporary folder {self.tmp_path}...")
-        zip_path, hdrs = urllib.request.urlretrieve(ADE20K_url, f"{self.tmp_path}/{dataset_name}.zip")
-
-        logger.info(f"Extracting {zip_path} to temporary folder {self.tmp_path}...")
-        with zipfile.ZipFile(f"{zip_path}", 'r') as z:
-            z.extractall(f"{self.tmp_path}")
-        self.input_data_path = zip_path[:-4]
 
     def subsample(self, N, policy="random"):
         """
         Subsamples from ADE20K: it considers all images which class intersects with imagenet classes.
         Args:
             N: number of wanted samples.
-            policy: type of policy :"random", "balanced" (TODO)
+            policy: type of policy: "random", "balanced".
+                    "random": subsamples N images from the images class intersects with imagenet classes.
+                    "balanced": subsamples N images from the images class intersects with imagenet classes, while keeping
+                                the frequencies of each class.
 
         Returns:
             selected_img_path: list of path to images we want to keep in the new dataset
         """
-        selected_img_path = []
-        logger.info("Subsampling ADE20K...")
+
+        intersecting_img = []
+        logger.info(f"Subsampling ADE20K with a {policy} strategy...")
+        img_in_class = defaultdict(list)
         for set_path in [self.train_path, self.val_path]:
             for letter in os.listdir(set_path):
-                logger.debug("Letter", letter)
+                logger.debug("Folder with letter", letter)
                 letter_path = os.path.join(set_path, letter)
-                ADE_letter_classes = os.listdir(letter_path)
-                for cur_class in ADE_letter_classes:
+                for cur_class in os.listdir(letter_path):
                    spaced_class = " ".join(cur_class.split("_"))
                    for imagenet_class in self.IMAGENET_CLASSES:
                        if spaced_class in imagenet_class.split(", "):
                            logger.debug(f"Keep ADE20K class: {spaced_class}, imagenet: {imagenet_class.split(', ')}")
                            class_path = os.path.join(letter_path, cur_class)
-                           for f in os.listdir(class_path):
-                               if f.endswith(".jpg"):
-                                   img_path = os.path.join(class_path, f)
-                                   selected_img_path.append(img_path)
-                                   self.in_annotations[img_path] = self.IMAGENET_CLASSES[imagenet_class]
+                           for file in os.listdir(class_path):
+                               if file.endswith(".jpg"):
+                                   img_path = os.path.join(class_path, file)
+                                   intersecting_img.append(img_path)
+                                   img_label = self.IMAGENET_CLASSES[imagenet_class]
+                                   self.in_annotations[img_path] = img_label
+                                   img_in_class[img_label].append(img_path)
+
+        if N >= len(intersecting_img):
+            logger.info("Number of intersecting images < N(Number of images we want to keep): keeping all intersecting images.")
+            return intersecting_img
 
         if policy == "random":
-            if N < len(selected_img_path):
-                selected_img_path = random.sample(selected_img_path, N)
+            selected_img_path = random.sample(intersecting_img, N)
+
+        elif policy == "balanced":
+            nb_total_img = len(intersecting_img)
+            selected_img_path = []
+            print(f"Number of total images {nb_total_img}")
+            for cur_class in img_in_class.keys():
+                nb_img_in_class = len(img_in_class[cur_class])
+                new_nb_img_in_class = min(nb_img_in_class, ceil((nb_img_in_class*N) / nb_total_img))
+                selected_img_path += random.sample(img_in_class[cur_class], new_nb_img_in_class)
+                print(f"Class {cur_class}, nb img in class {nb_img_in_class}, new nb img {new_nb_img_in_class}")
+                print(f"Frequency = {nb_img_in_class/nb_total_img}")
+
+            selected_img_path = random.sample(selected_img_path, N)
 
         return selected_img_path
 
@@ -339,6 +374,7 @@ def main():
     parser.add_argument("--type", type=str.lower, help="coco or imagenet", choices=["coco", "imagenet"])
     parser.add_argument("--dataset", type=str.lower, help="Kanter or ADE20K or other to implement", choices=["kanter", "ade20k"])
     parser.add_argument("-y", action="store_true", help="automatically answer yes to all questions. If on, the script may remove folders without permission.")
+    parser.add_argument("--subsampling_strategy", type=str.lower, help="random or balanced", choices=["random", "balanced"], default="random")
     args = parser.parse_args()
 
     input_data_path = args.input_data_path
@@ -354,7 +390,7 @@ def main():
                                 type=args.type,
                                 yes_all=args.y)
 
-    selected_img_path = input_dataset.subsample(N=args.N)
+    selected_img_path = input_dataset.subsample(N=args.N, policy=args.subsampling_strategy)
     input_dataset.process_dataset(selected_img_path=selected_img_path)
 
 
