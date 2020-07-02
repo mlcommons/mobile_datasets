1c5bd70d36e38ef04a8f008dfae0125d25449043
cindytrinh1 <cindy.trinh.pro@gmail.com>
2020-07-02 18:24:09 +0200

    add coco code, not tested on app yet

--- script.py
+++ script.py
@@ -37,8 +37,9 @@ import subprocess
 import logging
 from collections import defaultdict
 from math import ceil
-
-logging.basicConfig(level=logging.INFO)
+import matplotlib.pyplot as plt
+import numpy as np
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # CONSTANTS
@@ -84,8 +85,12 @@ class InputDataset:
         self.tmp_path = os.path.join(self.mobile_app_path, "tmp_dataset_script") # temporary folder
         self.check_tmp_path()
 
+        self.out_ann_path = os.path.join(self.mobile_app_path, "java", "org", "mlperf", "inference", "assets", self.type+"_val")
+        if self.type == "imagenet":
+            self.out_ann_path = self.out_ann_path + ".txt"
+        elif self.type == "coco":
+            self.out_ann_path = self.out_ann_path + ".pbtxt"
 
-        self.out_ann_path = os.path.join(self.mobile_app_path, "java", "org", "mlperf", "inference", "assets")
         self.out_img_path = os.path.join(self.tmp_path, "img")
         logger.info(f"Creating {self.out_img_path} directory")
         os.makedirs(self.out_img_path)
@@ -122,6 +127,10 @@ class InputDataset:
             logger.info("Loading imagenet classes")
             self.IMAGENET_CLASSES =  {v:k for (k,v) in eval(requests.get(IMAGENET_CLASSES_URL).text).items()}
             logger.debug(self.IMAGENET_CLASSES)
+        if self.type == "coco":
+            list_coco_classes = (requests.get(COCO_CLASSES_URL).text).split("\n")
+            self.COCO_CLASSES = dict(zip(list_coco_classes, [i+1 for i in range(len(list_coco_classes))]))
+            self.COCO_CLASSES_reverse = dict(zip([i+1 for i in range(len(list_coco_classes))],list_coco_classes))
 
     def download_dataset(self):
         """
@@ -150,21 +159,20 @@ class InputDataset:
             selected_img_path: list[str]
                 list of paths to images that we want to put in the new dataset
         """
-        #### Process data locally ####
-        if self.type == "coco":
-            out_ann_name = os.path.join(self.out_ann_path, "coco_val.pbtxt")
-            raise NotImplementedError
-        elif self.type == "imagenet":
-            new_ann_path = os.path.join(self.out_ann_path, "imagenet_val.txt")
-            with open(new_ann_path,'w') as new_ann_file:
-                for i, img_path in enumerate(selected_img_path):
-                    #self.convert_img(img_path): not useful for ADE20K (already jpg image) but might be necessary for other datasets
-                    label = self.find_label(img_path)
+
+        with open(self.out_ann_path,'w') as new_ann_file:
+            for i, img_path in enumerate(selected_img_path):
+                if self.type == "imagenet":
                     new_img_name = f"ILSVRC2012_val_{i+1:08}.JPEG"
-                    logger.debug(f"Copying {img_path} to \n {os.path.join(self.out_img_path, new_img_name)}")
-                    shutil.copyfile(img_path,
-                                    os.path.join(self.out_img_path, new_img_name))
-                    new_ann_file.write(str(label) + "\n")
+                elif self.type == "coco":
+                    new_img_name = f"{i+1:012}.jpg"
+                logger.debug(f"Copying {img_path} to \n {os.path.join(self.out_img_path, new_img_name)}")
+                shutil.copyfile(img_path,
+                                os.path.join(self.out_img_path, new_img_name))
+
+                self.write_annotation(new_ann_file=new_ann_file, img_path=img_path, new_img_name=new_img_name)
+
+
 
         #### Push to mobile ####
         mobile_dataset_path = os.path.join(os.sep,'sdcard','mlperf_datasets', self.type)
@@ -211,7 +219,7 @@ class InputDataset:
             logger.info(f"{self.tmp_path} folder has been removed.")
 
 
-
+            #TODO: remove find_label
     def find_label(self, img_path):
         """
         Given an image path, returns the ground-truth class.
@@ -230,6 +238,7 @@ class InputDataset:
 
 
 
+
 class ADE20KDataset(InputDataset):
     def __init__(self, input_data_path, mobile_app_path, type, yes_all):
         super().__init__(input_data_path=input_data_path,
@@ -239,6 +248,44 @@ class ADE20KDataset(InputDataset):
         self.train_path = os.path.join(self.input_data_path, "images", "training")
         self.val_path = os.path.join(self.input_data_path, "images", "validation")
         self.in_annotations = {}
+
+    def load_classes(self):
+        super().load_classes()
+        from scipy.io import loadmat
+        mat_path = os.path.join(self.input_data_path, "index_ade20k.mat")
+        object_names = loadmat(mat_path)['index']['objectnames'][0][0][0]
+        self.ADE20K_CLASSES = {}
+        self.ADE20K_CLASSES_reverse = {}
+        for i in range(len(object_names)):
+            self.ADE20K_CLASSES[object_names[i][0]] = i+1
+            self.ADE20K_CLASSES_reverse[i+1] = object_names[i][0]
+            if i == 3:
+                logger.debug(f"ADE20K_CLASSES: {self.ADE20K_CLASSES}")
+                logger.debug(f"ADE20K_CLASSES_reverse: {self.ADE20K_CLASSES_reverse}")
+
+    def is_in_imagenet(self, ade_class):
+        """
+        Checks if ade_class belongs to imagenet. If so, returns the corresponding imagenet class index.
+        """
+        spaced_class = " ".join(ade_class.split("_"))
+        for imagenet_class in self.IMAGENET_CLASSES:
+           if spaced_class in imagenet_class.split(", "):
+               logger.debug(f"ADE20K class: {spaced_class} is in imagenet: {imagenet_class.split(', ')}")
+               return self.IMAGENET_CLASSES[imagenet_class]
+        return None
+
+    def is_in_coco(self, ade_class_idx):
+        """
+        Checks if ade_class belongs to coco. If so, returns the corresponding coco class index.
+        """
+        if ade_class_idx == 0:
+            return None
+
+        for single_label in self.ADE20K_CLASSES_reverse[ade_class_idx].split(", "):
+            if single_label in self.COCO_CLASSES.keys():
+                return self.COCO_CLASSES[single_label]
+        return None
+
 
     def download_dataset(self):
         dataset_name = ADE20K_URL.split("/")[-1].split(".")[0]
@@ -278,26 +325,56 @@ class ADE20KDataset(InputDataset):
         """
 
         intersecting_img = []
-        logger.info(f"Subsampling ADE20K with a {policy} strategy...")
+        logger.info(f"Subsampling ADE20K with a {policy} policy...")
         img_in_class = defaultdict(list)
         for set_path in [self.train_path, self.val_path]:
             for letter in os.listdir(set_path):
-                logger.debug("Folder with letter", letter)
+                logger.debug(f"Folder with letter {letter}")
                 letter_path = os.path.join(set_path, letter)
                 for cur_class in os.listdir(letter_path):
-                   spaced_class = " ".join(cur_class.split("_"))
-                   for imagenet_class in self.IMAGENET_CLASSES:
-                       if spaced_class in imagenet_class.split(", "):
-                           logger.debug(f"Keep ADE20K class: {spaced_class}, imagenet: {imagenet_class.split(', ')}")
-                           class_path = os.path.join(letter_path, cur_class)
-                           for file in os.listdir(class_path):
-                               if file.endswith(".jpg"):
-                                   img_path = os.path.join(class_path, file)
-                                   intersecting_img.append(img_path)
-                                   img_label = self.IMAGENET_CLASSES[imagenet_class]
-                                   self.in_annotations[img_path] = img_label
-                                   img_in_class[img_label].append(img_path)
+                    class_path = os.path.join(letter_path, cur_class)
 
+                    if self.type == "imagenet":
+                        imagenet_label = self.is_in_imagenet(ade_class=cur_class)
+                        if imagenet_label is not None:
+                            for file in [f for f in os.listdir(class_path) if f.endswith(".jpg")]:
+                                img_path = os.path.join(class_path, file)
+                                intersecting_img.append(img_path)
+                                self.in_annotations[img_path] = imagenet_label
+                                img_in_class[imagenet_label].append(img_path)
+
+                    elif self.type == "coco":
+                        for seg_file in [f for f in os.listdir(class_path) if f.endswith('_seg.png')]:
+                            img_path = os.path.join(class_path, seg_file[:-8] + ".jpg")
+                            seg_img = plt.imread(os.path.join(class_path, seg_file))
+
+                            n_row, n_col = seg_img.shape[0], seg_img.shape[1]
+                            instance_seg_img = (seg_img[:,:,2]*255).astype("int")
+                            class_seg_img = (seg_img[:,:,0]*255/10*256 + seg_img[:,:,1]*255).astype("int")
+                            label_correspondance = {}
+                            for ade_idx in np.unique(class_seg_img):
+                                coco_label = self.is_in_coco(ade_idx)
+                                if coco_label is not None:
+                                    label_correspondance[ade_idx] = coco_label
+                            if label_correspondance:
+                                logger.debug(f"{img_path}, label correspondance {label_correspondance}")
+                                self.in_annotations[img_path] = {}
+                                for r in range(n_row):
+                                    for c in range(n_col):
+                                        if class_seg_img[r,c] in label_correspondance:
+                                            object_id = instance_seg_img[r,c]
+                                            if object_id not in self.in_annotations[img_path]:
+                                                self.in_annotations[img_path][object_id] = {}
+                                                self.in_annotations[img_path][object_id]["label"] = label_correspondance[class_seg_img[r,c]]
+                                                self.in_annotations[img_path][object_id]["normalized_bbox"] = [r/255, r/255, c/255, c/255]
+                                            else:
+                                                self.in_annotations[img_path][object_id]["normalized_bbox"] = [min(self.in_annotations[img_path][object_id]["normalized_bbox"][0], r/255),
+                                                                                                               max(self.in_annotations[img_path][object_id]["normalized_bbox"][1], r/255),
+                                                                                                               min(self.in_annotations[img_path][object_id]["normalized_bbox"][2], c/255),
+                                                                                                               max(self.in_annotations[img_path][object_id]["normalized_bbox"][3], c/255)]
+                                intersecting_img.append(img_path)
+
+        logger.info(f"Number of intersecting images : {len(intersecting_img)}")
         if N >= len(intersecting_img):
             logger.info("Number of intersecting images < N(Number of images we want to keep): keeping all intersecting images.")
             return intersecting_img
@@ -306,26 +383,45 @@ class ADE20KDataset(InputDataset):
             selected_img_path = random.sample(intersecting_img, N)
 
         elif policy == "balanced":
-            nb_total_img = len(intersecting_img)
-            selected_img_path = []
-            print(f"Number of total images {nb_total_img}")
-            for cur_class in img_in_class.keys():
-                nb_img_in_class = len(img_in_class[cur_class])
-                new_nb_img_in_class = min(nb_img_in_class, ceil((nb_img_in_class*N) / nb_total_img))
-                selected_img_path += random.sample(img_in_class[cur_class], new_nb_img_in_class)
-                print(f"Class {cur_class}, nb img in class {nb_img_in_class}, new nb img {new_nb_img_in_class}")
-                print(f"Frequency = {nb_img_in_class/nb_total_img}")
+            if self.type == "imagenet":
+                nb_total_img = len(intersecting_img)
+                selected_img_path = []
+                print(f"Number of total images {nb_total_img}")
+                for cur_class in img_in_class.keys():
+                    nb_img_in_class = len(img_in_class[cur_class])
+                    new_nb_img_in_class = min(nb_img_in_class, ceil((nb_img_in_class*N) / nb_total_img))
+                    selected_img_path += random.sample(img_in_class[cur_class], new_nb_img_in_class)
+                    print(f"Class {cur_class}, nb img in class {nb_img_in_class}, new nb img {new_nb_img_in_class}")
+                    print(f"Frequency = {nb_img_in_class/nb_total_img}")
 
-            selected_img_path = random.sample(selected_img_path, N)
-
+                selected_img_path = random.sample(selected_img_path, N)
+            else:
+                raise NotImplementedError
         return selected_img_path
 
-    def find_label(self, img_path):
+    def write_annotation(self, new_ann_file, img_path, new_img_name):
+        """
+        Write annotations for img_path
+        à mettre dans ade20k class"""
         if self.type == "imagenet":
             label = self.in_annotations[img_path]
             logger.debug(f"Img {img_path}, imagenet label {label}")
-        return label
-
+            new_ann_file.write(str(label) + "\n")
+        elif self.type == "coco":
+            new_ann_file.write("detection_results {\n")
+            for obj in self.in_annotations[img_path].keys():
+                new_ann_file.write("  objects {\n")
+                new_ann_file.write(f"    class_id: {self.in_annotations[img_path][obj]['label']}\n")
+                new_ann_file.write("    bounding_box {\n")
+                new_ann_file.write(f"      normalized_top: {self.in_annotations[img_path][obj]['normalized_bbox'][0]}\n")
+                new_ann_file.write(f"      normalized_bottom: {self.in_annotations[img_path][obj]['normalized_bbox'][1]}\n")
+                new_ann_file.write(f"      normalized_left: {self.in_annotations[img_path][obj]['normalized_bbox'][2]}\n")
+                new_ann_file.write(f"      normalized_right: {self.in_annotations[img_path][obj]['normalized_bbox'][3]}\n")
+                new_ann_file.write("    }\n")
+                new_ann_file.write("  }\n")
+            new_ann_file.write(f'  image_name: "{new_img_name}"\n')
+            new_ann_file.write(f'  image_id: {int(new_img_name.split(".")[0])}\n')
+            new_ann_file.write("}\n")
 
 class KanterDataset(InputDataset):
     def __init__(self, input_data_path, mobile_app_path, type,yes_all):
@@ -368,10 +464,13 @@ def main():
     parser.add_argument("--mobile_app_path", type=str, help="path to root directory containing the mobile app repo")
     parser.add_argument("--N", type=int, help="number of samples wanted in the dataset")
     parser.add_argument("--type", type=str.lower, help="coco or imagenet", choices=["coco", "imagenet"])
-    parser.add_argument("--dataset", type=str.lower, help="Kanter or ADE20K or other to implement", choices=["kanter", "ade20k"])
+    parser.add_argument("--dataset", type=str.lower, default="ade20k", help="Kanter or ADE20K or other to implement", choices=["kanter", "ade20k"])
     parser.add_argument("-y", action="store_true", help="automatically answer yes to all questions. If on, the script may remove folders without permission.")
     parser.add_argument("--subsampling_strategy", type=str.lower, help="random or balanced", choices=["random", "balanced"], default="random")
     args = parser.parse_args()
+
+    subprocess.check_call(["adb", "devices"])
+
 
     input_data_path = args.input_data_path
     if args.dataset == "kanter":
